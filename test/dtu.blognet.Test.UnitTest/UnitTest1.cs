52148e03f39ff7a28638c36a88939ec0400f5d31
Mathias Petersen <map@it-minds.dk>
2017-08-13 23:08:11 +0200

    add initial test

---
+++ test/dtu.blognet.Test.UnitTest/UnitTest1.cs
@@ -0,0 +1,14 @@
+using System;
+using Xunit;
+
+namespace dtu.blognet.Test.UnitTest
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}

db6f6e17361386640da96bd0d22326c2bb3f5783
Mathias Petersen <map@it-minds.dk>
2017-08-21 19:06:49 +0200

    add BlogCommandHandleFactory

---
+++ src/dtu.blognet.Core.Command/CommandHandlerFactories/BlogCommandHandleFactory.cs
@@ -0,0 +1,22 @@
+﻿using dtu.blognet.Core.Command.CommandHandlers.BlogCommandHandlers;
+using dtu.blognet.Core.Command.Commands.BlogCommands;
+using dtu.blognet.Infrastructure.DataAccess;
+
+namespace dtu.blognet.Core.Command.CommandHandlerFactories
+{
+    public class BlogCommandHandleFactory
+    {
+      private readonly ApplicationDbContext _context;
+
+      public BlogCommandHandleFactory(ApplicationDbContext context)
+      {
+        _context = context;
+      }
+
+      public ICommandHandler<AddBlogCommand, CommandResponse> Build(AddBlogCommand command)
+      {
+        return new AddBlogCommandHandler(_context, command);
+      }
+      
+    }
+}

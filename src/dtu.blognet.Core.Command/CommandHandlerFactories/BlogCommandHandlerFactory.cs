20f5ba8a2b7225106da0602d4b1045974ad159fb
Mathias Petersen <map@it-minds.dk>
2017-08-23 02:45:51 +0200

    add IMappingInterface to command

--- src/dtu.blognet.Core.Command/CommandHandlerFactories/BlogCommandHandlerFactory.cs
+++ src/dtu.blognet.Core.Command/CommandHandlerFactories/BlogCommandHandlerFactory.cs
@@ -1,5 +1,8 @@
 ﻿using dtu.blognet.Core.Command.CommandHandlers.BlogCommandHandlers;
 using dtu.blognet.Core.Command.Commands.BlogCommands;
+using dtu.blognet.Core.Command.InputModels.BlogInputModels;
+using dtu.blognet.Core.Command.MappingInterfaces;
+using dtu.blognet.Core.Entities;
 using dtu.blognet.Infrastructure.DataAccess;
 
 namespace dtu.blognet.Core.Command.CommandHandlerFactories
@@ -7,15 +10,17 @@ namespace dtu.blognet.Core.Command.CommandHandlerFactories
     public class BlogCommandHandlerFactory
     {
         private readonly ApplicationDbContext _context;
-  
-        public BlogCommandHandlerFactory(ApplicationDbContext context)
-        {
-            _context = context;
-        }
+      private readonly IMappingInterface<BlogInputModel, Blog> _blogInputModel2Blog;
+
+      public BlogCommandHandlerFactory(ApplicationDbContext context, IMappingInterface<BlogInputModel, Blog> blogInputModel2Blog)
+      {
+        _context = context;
+        _blogInputModel2Blog = blogInputModel2Blog;
+      }
   
         public ICommandHandler<AddBlogCommand, CommandResponse> Build(AddBlogCommand command)
         {
-            return new AddBlogCommandHandler(_context, command);
+            return new AddBlogCommandHandler(_context, _blogInputModel2Blog, command);
         }
         
     }
--- src/dtu.blognet.Core.Command/CommandHandlers/BlogCommandHandlers/AddBlogCommandHandler.cs
+++ src/dtu.blognet.Core.Command/CommandHandlers/BlogCommandHandlers/AddBlogCommandHandler.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using dtu.blognet.Core.Command.Commands.BlogCommands;
+using dtu.blognet.Core.Command.InputModels.BlogInputModels;
+using dtu.blognet.Core.Command.MappingInterfaces;
 using dtu.blognet.Core.Entities;
 using dtu.blognet.Infrastructure.DataAccess;
 
@@ -7,30 +9,29 @@ namespace dtu.blognet.Core.Command.CommandHandlers.BlogCommandHandlers
 {
     public class AddBlogCommandHandler : BaseCommandHandler, ICommandHandler<AddBlogCommand, CommandResponse>
     {
+        private readonly IMappingInterface<BlogInputModel, Blog> _blogInputModel2Blog;
         private readonly AddBlogCommand _command;
-    
-        public AddBlogCommandHandler(ApplicationDbContext dbContext, AddBlogCommand command) : base(dbContext)
+
+        public AddBlogCommandHandler(ApplicationDbContext dbContext,
+            IMappingInterface<BlogInputModel, Blog> blogInputModel2Blog, AddBlogCommand command) : base(dbContext)
         {
+            _blogInputModel2Blog = blogInputModel2Blog;
             _command = command;
         }
-    
-    
+
+
         public CommandResponse Execute()
         {
-            var response = new CommandResponse()
+            var response = new CommandResponse
             {
                 Success = false
             };
-          
+
             using (var transaction = _dbContext.Database.BeginTransaction())
             {
-                // Should be automapper
-                var blog = new Blog
-                {
-                    Title = _command.Model.Title
-                };
                 try
                 {
+                    var blog = _blogInputModel2Blog.Convert(_command.Model);
                     _dbContext.Blogs.Add(blog);
                     _dbContext.SaveChanges();
                     transaction.Commit();
@@ -38,12 +39,12 @@ namespace dtu.blognet.Core.Command.CommandHandlers.BlogCommandHandlers
                 }
                 catch (Exception e)
                 {
-                  // LOG!!!
-                  Console.WriteLine(e);
-                  transaction.Rollback();
+                    // LOG!!!
+                    Console.WriteLine(e);
+                    transaction.Rollback();
                 }
-              }
-              return response;
+            }
+            return response;
         }
     }
 }

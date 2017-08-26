64fb8ee4c4e72d8d52df347505bba40c23c98f2a
Mathias Petersen <mathias.petersen95@gmail.com>
2017-08-26 15:12:39 +0200

    add descriptions to blogs

--- src/dtu.blognet.Core.Entities/Blog.cs
+++ src/dtu.blognet.Core.Entities/Blog.cs
@@ -1,8 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+
 namespace dtu.blognet.Core.Entities
 {
     public class Blog
     {
         public int Id { get; set; }
         public string Title { get; set; }
+        public string Description { get; set; }
+        public virtual ICollection<BlogTag> Tags { get; set; }
+        // TODO: add owner account.
+        // TODO: add subscribers 
     }
 }

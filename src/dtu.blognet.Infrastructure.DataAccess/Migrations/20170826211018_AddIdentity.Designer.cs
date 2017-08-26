ca6e7fd8afe47882dff52f3cb6730bb89649dfe4
Mathias Petersen <mathias.petersen95@gmail.com>
2017-08-26 23:10:54 +0200

    add Identity migration

--- src/dtu.blognet.Infrastructure.DataAccess/Migrations/20170826211018_AddIdentity.Designer.cs
+++ src/dtu.blognet.Infrastructure.DataAccess/Migrations/20170826211018_AddIdentity.Designer.cs
@@ -8,9 +8,10 @@ using dtu.blognet.Infrastructure.DataAccess;
 namespace dtu.blognet.Infrastructure.DataAccess.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20170826211018_AddIdentity")]
+    partial class AddIdentity
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
             modelBuilder
                 .HasAnnotation("ProductVersion", "1.1.2")

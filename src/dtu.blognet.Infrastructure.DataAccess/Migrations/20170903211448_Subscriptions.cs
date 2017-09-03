b0130ff1656a5b16d98f71d5aa593806b7a2f1a9
Mathias Petersen <mathias.petersen95@gmail.com>
2017-09-03 23:16:24 +0200

    add Subscriptions

---
+++ src/dtu.blognet.Infrastructure.DataAccess/Migrations/20170903211448_Subscriptions.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace dtu.blognet.Infrastructure.DataAccess.Migrations
+{
+    public partial class Subscriptions : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Subscriptions",
+                columns: table => new
+                {
+                    Id = table.Column<string>(nullable: false),
+                    AccountId = table.Column<string>(nullable: true),
+                    BlogId = table.Column<int>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Subscriptions", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Subscriptions_AspNetUsers_AccountId",
+                        column: x => x.AccountId,
+                        principalTable: "AspNetUsers",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                    table.ForeignKey(
+                        name: "FK_Subscriptions_Blogs_BlogId",
+                        column: x => x.BlogId,
+                        principalTable: "Blogs",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Subscriptions_AccountId",
+                table: "Subscriptions",
+                column: "AccountId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Subscriptions_BlogId",
+                table: "Subscriptions",
+                column: "BlogId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Subscriptions");
+        }
+    }
+}
